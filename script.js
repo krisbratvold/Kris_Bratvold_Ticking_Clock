f0925fcc38fbcee337c5d5ec48d7f6d5ba748711
krisbratvold <kris.bratvold@gmail.com>
2021-04-29 14:05:02 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+setInterval(setClock, 1000) 
+
+const hourHand = document.querySelector("[data-hour-hand]")
+const minuteHand = document.querySelector("[data-minute-hand]")
+const secondHand = document.querySelector("[data-second-hand]")
+
+function setClock(){
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes())/ 60
+    const hoursRatio = (minutesRatio + currentDate.getHours())/ 12
+    moveHands(secondHand, secondsRatio);
+    moveHands(minuteHand, minutesRatio);
+    moveHands(hourHand, hoursRatio)
+}
+
+function moveHands(element, rotationRatio){
+    element.style.setProperty("--rotation",rotationRatio *360)
+}
+
+setClock()
